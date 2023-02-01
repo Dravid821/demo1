452430b1f3b6aa21f023641dc2dc2f0607346ef3
Dravid821 <124051008+Dravid821@users.noreply.github.com>
2023-02-01 14:18:09 +0530

    commit this file1

---
+++ 1.js
@@ -0,0 +1,18 @@
+const express = require('express');
+var app=express();
+app.get('/Home',function(req,res){
+    res.send('welcome to Home Page');
+})
+app.get('/About',function(req,res){
+    res.send('welcome to About Page');
+})
+app.get('/View',function(req,res){
+    res.send('welcome to View Page');
+})
+app.get('/more1',function(req,res){
+    res.send('welcome tomore Page');
+})
+app.get('/Contact',function(req,res){
+    res.send('welcome to Contact Page');
+})
+app.listen(8000);
